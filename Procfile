758db04bc494381fc0a88cc3fbd8c44a6716fc89
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2019-12-23 18:42:56 -0300

    Revert "up"
    This reverts commit e9ffd0ff8fcd6ad321cad636a0a9ab30192e2669.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run server
+web: node ./src/_config/main
--- src/_config/bootstrap.js
+++ src/_config/bootstrap.js
@@ -1,4 +1,4 @@
-const server = require('../server');
+const server = require('../server-user');
 
 module.exports = () => {
     server();
